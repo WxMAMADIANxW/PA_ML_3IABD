ee0750d9c027f7cc2a1a61a26b01e5b25ed752fc
WxMAMADIANxW <mdjalo2@myges.fr>
2021-05-05 20:32:33 +0200

    Base Native Call Python

---
+++ CallNativePython/main.py
@@ -0,0 +1,9 @@
+from ctypes import *
+path_to_shared_library = ""
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    cdll.LoadLibrary(path_to_shared_library)
+
+
+
